745db13188d1b748eca9e542dcd57c3525ddcd9f
AashishShrestha97 <beingaashish177@gmail.com>
2025-08-21 20:36:18 +0545

    made some changes

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,6 +1,5 @@
 import Recipe from "../Components/Categories/Description-Recipe";
 import PopularItems from "../Components/Menu/PopularItems";
-import Card from "../Components/Categories/FirstCard";
 import ContactUs from "../Components/ContactUs";
 import Navbar from "../Components/Navbar";
 
@@ -8,9 +7,9 @@ const Categories=()=>{
     return(
         <>
         <Navbar/>
-        <Card/>
         <Recipe />
         <PopularItems />
+        
         <ContactUs/>
         </>
     )
